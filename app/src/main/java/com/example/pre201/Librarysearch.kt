44d013c3362e7bac07b70cc77220728a7bcd8615
s19001 <s19001@std.it-college.ac.jp>
2021-01-21 14:18:26 +0900

    first

--- app/src/main/java/com/example/pre201/Librarysearch.kt
+++ app/src/main/java/com/example/pre201/Librarysearch.kt
@@ -1,11 +1,19 @@
 package com.example.pre201
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_librarysearch.*
 
 class Librarysearch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_librarysearch)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/pre201/MainActivity.kt
+++ app/src/main/java/com/example/pre201/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pre201
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LibSrc.setOnClickListener{
+            val intent = Intent(this, Librarysearch::class.java)
+            startActivity(intent)
+        }
     }
 }
